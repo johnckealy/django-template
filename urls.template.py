9cfa6abda5df541841e88577bcf81ae7a73a474f
John Kealy <johnckealy@gmail.com>
2020-06-26 16:06:32 +0100

    first commit

---
+++ urls.template.py
@@ -0,0 +1,10 @@
+from django.urls import path, re_path
+# from .views import HomeView, ArticleView
+
+# app_name = ''
+urlpatterns = [
+    # path('', HomeView.as_view(), name='home'),
+    # path("articles/<int:pk>/something/", ArticleView.as_view(), name="article"),
+    # re_path(r'^windy/$', windy, name='windy'),
+]
+
---
+++ views.template.py
@@ -0,0 +1,15 @@
+# from django.views.generic import TemplateView
+# from app_name.models import Article
+from django.shortcuts import render
+
+
+# class HomeView(TemplateView):
+#     template_name = "app_name/index.html"
+
+
+
+# def windy(request):
+#     if request.method == 'GET':
+#         article = Article.objects.get(pk=query_string)
+        
+#     return render(request,'app_name/detail.html', {'article': article})
